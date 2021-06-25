853e1c6c9fe7379ce2c71c548786175e45fbec98
Zandra Monteiro <zandra.monteiro@liferay.com>
2021-06-25 13:41:19 -0300

    adjusting folders

--- src/com/zmonteiro/Main.java
+++ src/com/zmonteiro/Main.java
@@ -1,26 +1,10 @@
 package com.zmonteiro;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Main {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args){
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        Date birthDate = sdf.parse("16/02/1987");
-        long millis = birthDate.getTime();
-
-        if(birthDate.after(sdf.parse("15/05/2010"))){
-            System.out.println("Your birthdate is after 15/05/2010");
-        }
-        if(birthDate.before(sdf.parse("15/05/2010"))){
-            System.out.println("Your birthdate is before 15/05/2010");
-        } else{
-            System.out.println("You were born on 15/05/2010");
-        }
 
     }
 }
--- src/com/zmonteiro/birthdate/ExerciseDate.java
+++ src/com/zmonteiro/birthdate/ExerciseDate.java
@@ -6,5 +6,17 @@ import java.util.Date;
 
 public class ExerciseDate {
 
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+    Date birthDate = sdf.parse("16/02/1987");
+    long millis = birthDate.getTime();
+
+        if(birthDate.after(sdf.parse("15/05/2010"))){
+        System.out.println("Your birthdate is after 15/05/2010");
+    }
+        if(birthDate.before(sdf.parse("15/05/2010"))){
+        System.out.println("Your birthdate is before 15/05/2010");
+    } else{
+        System.out.println("You were born on 15/05/2010");
+    }
 }
